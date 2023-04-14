dc76257479e1febe6b8846fd4f61c8ef4e345f60
Sandeep <sandeep.megavath@westagilelabs.com>
2023-04-14 18:56:51 +0530

    Initial Commit

---
+++ TableViewBeginner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewBeginner
+//
+//  Created by West Agile labs on 14/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
